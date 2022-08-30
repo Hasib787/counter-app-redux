b4bdcead6f8e67c22accde43b6ac680f6d9aa3ab
Md. Masud Rana <mr.hasib45@gmail.com>
2022-08-31 02:54:01 +0600

    added reducer function

---
+++ script.js
@@ -0,0 +1,26 @@
+// Select dom elements
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+// initial state
+const initialState = {
+  value: 0,
+};
+
+// create reducer function
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+};
